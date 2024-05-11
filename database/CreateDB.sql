b8a6259948b916b41526dcf9cb1280d0deb806ba
TrulyFalse <159928596+TrulyFalse@users.noreply.github.com>
2024-05-11 10:43:43 +0600

    Update CreateDB.sql
    Fixed some syntax errors

--- database/CreateDB.sql
+++ database/CreateDB.sql
@@ -1,19 +1,21 @@
+/* code by Abrar Ur Alam 2211447042 */
+
 CREATE DATABASE Craving_DB;
 
 Use Craving_DB;
 
 CREATE TABLE account(
-    user_ID int AUTO INCREMENT,
+    user_ID int AUTO_INCREMENT,
 
     username varchar(25) UNIQUE NOT NULL,
     email varchar(25) UNIQUE NOT NULL,
     password varchar(50) NOT NULL,
 
     PRIMARY KEY(user_ID)
-    )
+);
 
 CREATE TABLE restaurant(
-    restaurant_ID int AUTO INCREMENT,
+    restaurant_ID int AUTO_INCREMENT,
 
     restaurant_name varchar(50),
     city varchar(25),
@@ -22,10 +24,10 @@ CREATE TABLE restaurant(
     closing_time TIME,
 
     PRIMARY KEY(restaurant_ID)
-)
+);
 
 CREATE TABLE food(
-    food_ID int AUTO INCREMENT,
+    food_ID int AUTO_INCREMENT,
 
     restaurant_ID int NOT NULL,
     food_name varchar(25) NOT NULL,
@@ -36,11 +38,11 @@ CREATE TABLE food(
     vegan boolean,
 
     PRIMARY KEY(food_ID),
-    FOREIGN KEY(restaurant_ID) REFERENCES restaurant
-)
+    FOREIGN KEY(restaurant_ID) REFERENCES restaurant(restaurant_ID)
+);
 
-CREATE TABLE order(
-    order_ID int AUTO INCREMENT,
+CREATE TABLE orders(
+    order_ID int AUTO_INCREMENT,
 
     user_ID varchar(25) NOT NULL,
     food_ID int NOT NULL,
@@ -49,7 +51,7 @@ CREATE TABLE order(
     quantity tinyint NOT NULL,
     date DATETIME NOT NULL,
 
-    PRIMARY KEY(preference_ID),
-    FOREIGN KEY(username) REFERENCES account(user_ID),
+    PRIMARY KEY(order_ID),
+    FOREIGN KEY(user_ID) REFERENCES account(user_ID),
     FOREIGN KEY(food_ID) REFERENCES food(food_ID)
-)
+);
